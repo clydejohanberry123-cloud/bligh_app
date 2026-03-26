e3fe8ae8a6f637edbabbb938592e39fc73616f05
Clyde Berry <263224712+clydejohanberry123-cloud@users.noreply.github.com>
2026-03-26 00:54:01 +0000

    This is V03

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-import streamlit as st  
+import streamlit as st 
+from streamlit_calendar import calendar 
+
+st.set_page_config(layout='wide')
 
 st.header('Hello World')
 
 st.markdown('This is a test!')
+
+calendar(
+    events=[],
+    options={
+        "height": "auto",         # full height
+        "expandRows": True,
+        "initialView": "dayGridMonth",
+        "headerToolbar": {
+            "left": "prev,next today",
+            "center": "title",
+            "right": "dayGridMonth,timeGridWeek"
+        },
+        "slotDuration": "12:00:00",
+        "slotLabelFormat": {
+            "hour": "numeric",
+            "minute": "2-digit",
+            "meridiem": "short"     
+        },
+    },
+)
